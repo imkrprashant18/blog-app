373db56d364d6be824aa6f0d13ac47f9b9d3891a
imkrprashant18 <prashantmahato77@gmail.com>
2025-01-15 17:42:26 +0545

    new setup

--- client/src/components/routes/public-routes.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-import { Navigate } from "react-router-dom";
-
-const PublicRoute = ({ children }) => {
-  const token = sessionStorage.getItem("accessToken");
-  return token ? <Navigate to="/dashboard" replace /> : children;
-};
-
-export default PublicRoute;
--- client/src/pages/404/404.jsx
+++
@@ -1,7 +0,0 @@
-import ErrorPage404 from "../../components/404-page/404-page";
-
-const ErrorPage = () => {
-  return <ErrorPage404 />;
-};
-
-export default ErrorPage;
